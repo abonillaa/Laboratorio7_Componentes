088646635562c54976321cd7167a93910fcb5e65
Andres Bonilla <abonillaa@ucenfotec.ac.cr>
2020-07-11 18:08:23 -0600

    Initial commit

---
+++ MongoApi/src/main/java/com/cenfotec/mongoapi/repository/ProfessionalRepository.java
@@ -0,0 +1,9 @@
+package com.cenfotec.mongoapi.repository;
+
+import java.util.List;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import com.cenfotec.mongoapi.domain.*;;
+
+public interface ProfessionalRepository extends MongoRepository<Professional, String> {
+	public List< Professional > findByName(String name);
+} 
